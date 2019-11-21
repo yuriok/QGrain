14249c8840f86644c30dd93774d50591ede10e31
Yuming Liu <liuyuming@ieecas.cn>
2019-11-21 16:52:19 +0800

    use data_manager to manager th data

---
+++ data_manager.py
@@ -0,0 +1,95 @@
+
+
+
+
+import numpy as np
+from PyQt5.QtCore import pyqtSignal, QThread, Qt, QObject
+
+import logging
+from resolvers import DistributionType
+from data import GrainSizeData, FittedData, SampleData
+from data_loader import DataLoader
+import os
+from typing import List
+from PyQt5.QtWidgets import QFileDialog
+
+class DataManager(QObject):
+    sigDataLoadingStrated = pyqtSignal(str) # filename
+    sigDataLoadingFinished = pyqtSignal(bool) # task result, if `True`, task succeed
+    sigDataLoaded = pyqtSignal(GrainSizeData)
+    sigTargetDataChanged = pyqtSignal(str, np.ndarray, np.ndarray)
+    sigDataRecorded = pyqtSignal(FittedData)
+    def __init__(self):
+        super().__init__()
+        self.grain_size_data = None  # type: GrainSizeData
+        self.current_fitted_data = None  # type: FittedData
+        self.recorded_data_list = [] # type: List[FittedData]
+        self.file_dialog = QFileDialog()
+        self.data_loader = DataLoader()
+        self.load_data_thread = QThread()
+        # move it before the signal-slots are connected
+        self.data_loader.moveToThread(self.load_data_thread)
+        self.load_data_thread.start()
+        
+        self.sigDataLoadingStrated.connect(self.data_loader.try_load_data)
+        self.data_loader.sigWorkFinished.connect(self.on_work_finished)
+
+        self.auto_record = True
+
+
+    def on_load_data_clicked(self):
+        filename, _ = self.file_dialog.getOpenFileName(
+            None, self.tr("Select Data File"), None, "*.xls; *.xlsx; *.csv")
+        logging.info(self.tr("File path is [{0}].").format(filename))
+        if filename is None or filename == "":
+            return
+        if not os.path.exists(filename):
+            return
+        self.sigDataLoadingStrated.emit(filename)
+
+
+    def on_work_finished(self, grain_size_data: GrainSizeData):
+        if grain_size_data is not None:
+            self.grain_size_data = grain_size_data
+            self.sigDataLoadingFinished.emit(True)
+            self.sigDataLoaded.emit(grain_size_data)
+        else:
+            self.sigDataLoadingFinished.emit(False)
+
+    def on_focus_sample_changed(self, index: int):
+        if self.grain_size_data is None:
+            return
+        sample_name = self.grain_size_data.sample_data_list[index].name
+        classes = self.grain_size_data.classes
+        sample_data = self.grain_size_data.sample_data_list[index].distribution
+
+        self.sigTargetDataChanged.emit(sample_name, classes, sample_data)
+
+
+    def on_epoch_finished(self, data: FittedData):
+
+        print("Statistic for {0}:\n".format(data.name) +
+              "|{0:12}|{1:12}|{2:12}|{3:12}|{4:12}|{5:12}|{6:24}|{7:12}|{8:12}|\n".format(
+                  "Component", "Fraction", "Mean (μm)", "Median (μm)", "Mode (μm)", "Variance", "Standard Deviation", "Skewness", "Kurtosis") +
+              "\n".join(["|{0:12}|{1:<12.2f}|{2:<12.2f}|{3:<12.2f}|{4:<12.2f}|{5:<12.2f}|{6:<24.2f}|{7:<12.2f}|{8:<12.2f}|".format(
+                  i.get("name"), i.get("fraction"), i.get("mean"), i.get("median"), i.get("mode"), i.get("variance"), i.get("standard_deviation"), i.get("skewness"), i.get("kurtosis")) for i in data.statistic]))
+        print("Mean Squared Error:", data.mse)
+        self.current_fitted_data = data
+
+        if self.auto_record:
+            self.record_data()
+
+    def record_data(self):
+        self.recorded_data_list.append(self.current_fitted_data)
+        self.sigDataRecorded.emit(self.current_fitted_data)
+        
+    def on_settings_changed(self, kwargs: dict):
+        for setting, value in kwargs.items():
+            self.__setattr__(setting, value)
+
+    def remove_data(self, rows: List[int]):
+        offset = 0
+        for row in rows:
+            value_to_remove = self.recorded_data_list[row-offset]
+            self.recorded_data_list.remove(value_to_remove)
+            offset+=1
