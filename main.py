d1042463c142a47cb4b27da143caa675f694cc9e
Yuming Liu <695983364@qq.com>
2019-11-21 16:56:21 +0800

    add logging

--- main.py
+++ main.py
@@ -11,17 +11,25 @@ from pyqtgraph.parametertree import ParameterTree
 from pyqtgraph.parametertree.parameterTypes import GroupParameter, Parameter
 from scipy.optimize import minimize
 
-from ui import MainWindow
+from ui import MainWindow, GUILogHandler
+import logging
 
 # pg.setConfigOptions(background=pg.mkColor("#ffffff00"), foreground=pg.mkColor("#000000ff"))
 
 
 
 if __name__ == "__main__":
-    QCoreApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+    # TODO: fix the problem that when use high dpi scaling, the dock bar will not display the title correctly.
+    # May be it's related to QSS
+    # QCoreApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
     app = QApplication(sys.argv)
-    mainWindow = MainWindow()
-    mainWindow.setGeometry(200, 200, 900, 600)
-    mainWindow.show()
+    main_window = MainWindow()
+
+
+    
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logging.getLogger().addHandler(GUILogHandler(main_window))
+    
+    main_window.show()
 
     sys.exit(app.exec_())
