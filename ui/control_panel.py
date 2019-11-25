c33843e27c235d49d47bd0279a2f0cea3e9ee2bd
Yuming Liu <liuyuming2024@gmail.com>
2019-11-25 14:02:11 +0800

    add loggers and tool tips

--- ui/control_panel.py
+++ ui/control_panel.py
@@ -1,13 +1,16 @@
 import logging
 import os
+from enum import Enum
 
 import numpy as np
 from PySide2.QtCore import Qt, QThread, QTimer, Signal
-from PySide2.QtWidgets import (QCheckBox, QFileDialog, QGridLayout, QLabel, QMessageBox,
-                             QPushButton, QRadioButton, QSizePolicy, QWidget)
 from PySide2.QtGui import QFont
+from PySide2.QtWidgets import (QCheckBox, QFileDialog, QGridLayout, QLabel,
+                               QMessageBox, QPushButton, QRadioButton,
+                               QSizePolicy, QWidget)
+
 from data import FittedData, GrainSizeData
-from enum import Enum
+
 
 class ControlPanel(QWidget):
     sigDistributionTypeChanged = Signal(Enum)
@@ -17,7 +20,10 @@ class ControlPanel(QWidget):
     sigRuningSettingsChanged = Signal(dict)
     sigDataSettingsChanged = Signal(dict)
     sigRecordFittingData = Signal(str)
-    
+    logger = logging.getLogger("root.ui.ControlPanel")
+    gui_logger = logging.getLogger("GUI")
+
+
     def __init__(self, parent=None, **kargs):
         super().__init__(parent, **kargs)
         self.__ncomp = 2
@@ -35,63 +41,68 @@ class ControlPanel(QWidget):
         self.msg_box = QMessageBox()
         self.msg_box.setWindowFlags(Qt.Drawer)
 
-        # self.setStyleSheet("""QLabel, QPushButton, QRadioButton, QCheckBox {font: arial, sans-serif; font-size: 16px; font-weight: bold}""")
-
-
     def init_ui(self):
-        
         self.main_layout = QGridLayout(self)
-        
-        self.distribution_type_label = QLabel("Distribution Type:", self)
-        # self.distribution_type_label.setFont(QFont("Times New Roman"))
-        self.distribution_weibull_radio_button = QRadioButton("Weibull", self)
-        self.distribution_weibull_radio_button.setChecked(True)
-        self.distribution_lognormal_radio_button = QRadioButton("Lognormal", self)
+
+        self.distribution_type_label = QLabel(self.tr("Distribution Type:"), self)
+        self.distribution_type_label.setToolTip(self.tr("Select the base distribution function of each modal."))
+        self.distribution_weibull_radio_button = QRadioButton(self.tr("Weibull"), self)
+        self.distribution_weibull_radio_button.setToolTip(self.tr("See [%s] for more details.") % "https://en.wikipedia.org/wiki/Weibull_distribution")
+        self.distribution_lognormal_radio_button = QRadioButton(self.tr("Lognormal"), self)
+        self.distribution_lognormal_radio_button.setToolTip(self.tr("See [%s] for more details.") % "https://en.wikipedia.org/wiki/Log-normal_distribution")
         self.main_layout.addWidget(self.distribution_type_label, 0, 0, 1, 2)
         self.main_layout.addWidget(self.distribution_weibull_radio_button, 0, 2)
         self.main_layout.addWidget(self.distribution_lognormal_radio_button, 0, 3)
-        # TODO: Move when the lognormal is added
-        self.distribution_weibull_radio_button.setEnabled(False)
-        self.distribution_lognormal_radio_button.setEnabled(False)
 
-        self.ncomp_label = QLabel("Components:")
-        self.ncomp_display = QLabel("Unknown")
-        
-        self.ncomp_add_button = QPushButton("+")
-        self.ncomp_reduce_button = QPushButton("-")
-        
+        # Component number
+        self.ncomp_label = QLabel(self.tr("Components:"))
+        self.ncomp_label.setToolTip(self.tr("Select the mixed component number of the distribution."))
+        self.ncomp_display = QLabel(self.tr("Unknown"))
+        self.ncomp_add_button = QPushButton(self.tr("+"))
+        self.ncomp_add_button.setToolTip(self.tr("Click to add one component. It should be less than 11."))
+        self.ncomp_reduce_button = QPushButton(self.tr("-"))
+        self.ncomp_reduce_button.setToolTip(self.tr("Click to reduce one component. It should be greater than 1."))
         self.main_layout.addWidget(self.ncomp_label, 1, 0)
         self.main_layout.addWidget(self.ncomp_display, 1, 1)
         self.main_layout.addWidget(self.ncomp_add_button, 1, 2)
         self.main_layout.addWidget(self.ncomp_reduce_button, 1, 3)
 
-
-        self.show_iteration_checkbox = QCheckBox("Iteration Scope")
-        self.show_iteration_checkbox.setCheckState(Qt.Unchecked)
-        self.inherit_params_checkbox = QCheckBox("Inherit Parameters")
-        self.inherit_params_checkbox.setCheckState(Qt.Checked)
-        self.main_layout.addWidget(self.show_iteration_checkbox, 2, 0, 1, 2)
+        # Some usual settings
+        self.iteration_scope_checkbox = QCheckBox(self.tr("Iteration Scope"))
+        self.iteration_scope_checkbox.setToolTip(self.tr("When this option is checked, it will display the iteration procedure."))
+        self.inherit_params_checkbox = QCheckBox(self.tr("Inherit Parameters"))
+        self.inherit_params_checkbox.setToolTip(self.tr("When this option is checked, it will inherit the last fitted parameters to the next time of fitting.\nIt will highly improve the accuracy and efficiency when the samples are continuous."))
+        self.main_layout.addWidget(self.iteration_scope_checkbox, 2, 0, 1, 2)
         self.main_layout.addWidget(self.inherit_params_checkbox, 2, 2, 1, 2)
-        self.auto_fit_checkbox = QCheckBox("Auto Fit")
-        self.auto_fit_checkbox.setCheckState(Qt.Checked)
-        self.auto_record_checkbox = QCheckBox("Auto Record")
-        self.auto_record_checkbox.setCheckState(Qt.Checked)
+        self.auto_fit_checkbox = QCheckBox(self.tr("Auto Fit"))
+        self.auto_fit_checkbox.setToolTip(self.tr("When this option is checked, it will automaticlly fit after the sample data changed."))
+        self.auto_record_checkbox = QCheckBox(self.tr("Auto Record"))
+        self.auto_record_checkbox.setToolTip(self.tr("When this option is checked, it will automaticlly record the fitted data after fitting finished."))
         self.main_layout.addWidget(self.auto_fit_checkbox, 3, 0, 1, 2)
         self.main_layout.addWidget(self.auto_record_checkbox, 3, 2, 1, 2)
 
-        self.data_index_label = QLabel("Current Sample:")
-        self.data_index_display = QLabel("Unknown")
-        self.data_index_previous_button = QPushButton("Previous")
-        self.data_index_next_button = QPushButton("Next")
+        # Target data to fit
+        self.data_index_label = QLabel(self.tr("Current Sample:"))
+        self.data_index_label.setToolTip(self.tr("Current sample to fit."))
+        self.data_index_display = QLabel(self.tr("Unknown"))
+        self.data_index_previous_button = QPushButton(self.tr("Previous"))
+        self.data_index_previous_button.setToolTip(self.tr("Click to back to the previous sample."))
+        self.data_index_next_button = QPushButton(self.tr("Next"))
+        self.data_index_next_button.setToolTip(self.tr("Click to jump to the next sample."))
         self.main_layout.addWidget(self.data_index_label, 4, 0)
         self.main_layout.addWidget(self.data_index_display, 4, 1)
         self.main_layout.addWidget(self.data_index_previous_button, 4, 2)
         self.main_layout.addWidget(self.data_index_next_button, 4, 3)
 
-        self.auto_run = QPushButton("Auto Run")
-        self.cancel_run = QPushButton("Cancel")
-        self.try_fit_button = QPushButton("Try Fit")
-        self.record_button = QPushButton("Record")
+        # Control bottons
+        self.auto_run = QPushButton(self.tr("Auto Run"))
+        self.auto_run.setToolTip(self.tr("Click to auto run the program.\nThe samples from current to the end will be processed one by one."))
+        self.cancel_run = QPushButton(self.tr("Cancel"))
+        self.cancel_run.setToolTip(self.tr("Click to cancel the auto run.\n"))
+        self.try_fit_button = QPushButton(self.tr("Try Fit"))
+        self.try_fit_button.setToolTip(self.tr("Click to fit the current sample manually.\n"))
+        self.record_button = QPushButton(self.tr("Record"))
+        self.record_button.setToolTip(self.tr("Click to record the current fitted data manually.\nNote: It will only record the LAST fitted data, NOT CURRENT SAMPLE. \n"))
         self.main_layout.addWidget(self.auto_run, 5, 0)
         self.main_layout.addWidget(self.cancel_run, 5, 1)
         self.main_layout.addWidget(self.try_fit_button, 5, 2)
@@ -104,17 +115,13 @@ class ControlPanel(QWidget):
         self.data_index_previous_button.clicked.connect(self.on_data_index_previous_clicked)
         self.data_index_next_button.clicked.connect(self.on_data_index_next_clicked)
         
-        
-        self.show_iteration_checkbox.stateChanged.connect(self.on_show_iteration_changed)
+        self.iteration_scope_checkbox.stateChanged.connect(self.on_show_iteration_changed)
         self.inherit_params_checkbox.stateChanged.connect(self.on_inherit_params_changed)
         self.auto_fit_checkbox.stateChanged.connect(self.on_auto_fit_changed)
         self.auto_record_checkbox.stateChanged.connect(self.on_auto_record_changed)
 
-
         self.auto_run.clicked.connect(self.on_auto_run_clicked)
         self.cancel_run.clicked.connect(self.on_cancel_run_clicked)
-
-        
 
     @property
     def ncomp(self):
@@ -124,37 +131,41 @@ class ControlPanel(QWidget):
     def ncomp(self, value: int):
         # check the validity
         # ncomp should be non-negative
-        # and it's no need to unmix if ncomp is 1
         # TODO: change the way to generate plot styles in `FittingCanvas`, and remove the limit of <=10
         if value <= 1 or value > 10:
-            logging.warning("The component number should be > 1 and <= 10.")
+            self.gui_logger.info(self.tr("The component number should be > 1 and <= 10."))
             return
         # update the label to display the value
         self.ncomp_display.setText(str(value))
         self.__ncomp = value
         self.sigNcompChanged.emit(value)
+        # auto emit target data change signal again when ncomp changed
         if self.__sample_names is not None:
             self.data_index = self.data_index
-        
 
     @property
-    def data_index(self):
+    def data_index(self) -> int:
         return self.__data_index
 
     @data_index.setter
     def data_index(self, value: int):
         if self.__sample_names is None or len(self.__sample_names) == 0:
-            self.msg_box.setWindowTitle("Warning")
-            self.msg_box.setText("The data has not been loaded, the operation is invalid.")
+            self.msg_box.setWindowTitle(self.tr("Warning"))
+            self.msg_box.setText(self.tr("The data has not been loaded, the operation is invalid."))
             self.msg_box.exec_()
             return
         if value < 0 or value >= len(self.__sample_names):
-            logging.warning("It has reached the first/last sample.")
+            self.gui_logger.info(self.tr("It has reached the first/last sample."))
             return
         # update the label to display the name of this sample
         self.data_index_display.setText(self.__sample_names[value])
         self.__data_index = value
+        self.logger.debug("Data index has been set to [%d].", value)
         self.sigFocusSampleChanged.emit(value)
+
+    @property
+    def current_name(self) -> str:
+        return self.__sample_names[self.data_index]
 
     def on_ncomp_add_clicked(self):
         self.ncomp += 1
@@ -164,7 +175,6 @@ class ControlPanel(QWidget):
 
     def on_data_index_previous_clicked(self):
         self.data_index -= 1
-        
 
     def on_data_index_next_clicked(self):
         self.data_index += 1
@@ -198,19 +208,24 @@ class ControlPanel(QWidget):
 
     def on_data_loaded(self, grain_size_data: GrainSizeData):
         self.__sample_names = [sample.name for sample in grain_size_data.sample_data_list]
+        self.logger.debug("Data was loaded.")
         self.data_index = 0
+        self.logger.debug("Data index has been set to 0.")
 
     def on_data_selected(self, index):
         self.data_index = index
+        self.logger.debug("Sample data at [%d] is selected.", index)
 
     def on_widgets_enable_changed(self, enable: bool):
+        if self.auto_run_flag and enable:
+            return
         # self.distribution_weibull_radio_button.setEnabled(enable)
         # self.distribution_lognormal_radio_button.setEnabled(enable)
         self.ncomp_add_button.setEnabled(enable)
         self.ncomp_reduce_button.setEnabled(enable)
         self.data_index_previous_button.setEnabled(enable)
         self.data_index_next_button.setEnabled(enable)
-        self.show_iteration_checkbox.setEnabled(enable)
+        self.iteration_scope_checkbox.setEnabled(enable)
         self.inherit_params_checkbox.setEnabled(enable)
         self.auto_fit_checkbox.setEnabled(enable)
         self.auto_record_checkbox.setEnabled(enable)
@@ -220,20 +235,22 @@ class ControlPanel(QWidget):
         self.try_fit_button.setEnabled(enable)
         self.record_button.setEnabled(enable)
 
-
     def on_record_clickedd(self):
-        self.sigRecordFittingData.emit(self.__sample_names[self.data_index])
-
+        self.sigRecordFittingData.emit(self.current_name)
+        self.logger.debug("Record data signal emitted.")
 
     def on_epoch_finished(self, data: FittedData):
         if data.has_nan():
-            self.msg_box.setWindowTitle("Warning")
-            self.msg_box.setText("The fitted data may be invalid. It contains invalid values.")
-            self.msg_box.exec_()
+            self.logger.warning("The fitted data may be not valid, auto run stoped.")
+            self.gui_logger.warning(self.tr("The fitted data may be not valid, auto run stoped."))
             self.auto_run_flag = False
+            self.on_widgets_enable_changed(True)
         
         if self.data_index == len(self.__sample_names)-1:
+            self.logger.info("The auto run has reached the last sample and stoped.")
+            self.gui_logger.info(self.tr("The auto run has reached the last sample and stoped."))
             self.auto_run_flag = False
+            self.on_widgets_enable_changed(True)
 
         if self.auto_run_flag:
             self.auto_run_timer.start(5)
@@ -242,11 +259,22 @@ class ControlPanel(QWidget):
         self.data_index += 1
 
     def on_auto_run_clicked(self):
-        self.auto_run_flag = True
         # from current sample to fit, to avoid that it need to resart from the first sample every time
         self.data_index = self.data_index
-
+        self.auto_run_flag = True
+        self.logger.debug("Auto run started from sample [%s].", self.current_name)
 
     def on_cancel_run_clicked(self):
-        if self.auto_run_flag:
-            self.auto_run_flag = False
+        self.auto_run_flag = False
+        self.logger.debug("Auto run was canceled.")
+
+    def init_conditions(self):
+        self.ncomp = 3
+        self.distribution_weibull_radio_button.setChecked(True)
+        # TODO: Move when the lognormal is added
+        self.distribution_weibull_radio_button.setEnabled(False)
+        self.distribution_lognormal_radio_button.setEnabled(False)
+        self.iteration_scope_checkbox.setCheckState(Qt.Unchecked)
+        self.inherit_params_checkbox.setCheckState(Qt.Checked)
+        self.auto_fit_checkbox.setCheckState(Qt.Checked)
+        self.auto_record_checkbox.setCheckState(Qt.Checked)
--- ui/fitting_canvas.py
+++ ui/fitting_canvas.py
@@ -1,94 +1,104 @@
+import logging
 import math
 
 import numpy as np
 import pyqtgraph as pg
 from PySide2.QtCore import Qt, Signal
+from PySide2.QtGui import QFont
 from PySide2.QtWidgets import QGridLayout, QSizePolicy, QWidget
 
 from data import FittedData
 
-pg.setConfigOptions(foreground=pg.mkColor("k"), background=pg.mkColor("w"), antialias=True, )
+pg.setConfigOptions(foreground=pg.mkColor("k"), background=pg.mkColor("#FFFFFF00"), antialias=True)
 
 class FittingCanvas(QWidget):
     sigWidgetsEnable = Signal(bool)
-
+    logger = logging.getLogger("root.ui.FittingCanvas")
+    gui_logger = logging.getLogger("GUI")
+    
     def __init__(self, parent=None, **kargs):
         super().__init__(parent, **kargs)
         self.current_iteration = 0
-        self.sample_id = "Unknown"
+        self.sample_id = self.tr("Unknown")
         self.init_ui()
 
     def init_ui(self):
         self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.main_layout = QGridLayout(self)
-        self.plot_widget = pg.PlotWidget(
-            title="Fitting Windows", enableMenu=True)
+        self.plot_widget = pg.PlotWidget(enableMenu=True)
         self.main_layout.addWidget(self.plot_widget)
-
-        self.target_style = dict(pen=None, symbol="o", symbolBrush=pg.mkBrush(
-            "y"), symbolPen=None, symbolSize=6)
+        self.target_style = dict(pen=None, symbol="o", symbolBrush=pg.mkBrush("#161B26"), symbolPen=None, symbolSize=5)
         self.target_item = pg.PlotDataItem(name="Target", **self.target_style)
         self.plot_widget.plotItem.addItem(self.target_item)
-
-        self.sum_style = dict(pen=pg.mkPen("b", width=3, style=Qt.DashLine))
+        
+        self.sum_style = dict(pen=pg.mkPen("#062170", width=3, style=Qt.DashLine))
         self.sum_item = pg.PlotDataItem(name="Fitted", **self.sum_style)
         self.plot_widget.plotItem.addItem(self.sum_item)
-        self.plot_widget.plotItem.setLabels(
-            left="Probability Density", bottom="Grain size (μm)")
+        self.label_styles = {"font-family": "Times New Roman"}
+        self.plot_widget.plotItem.setLabel("left", self.tr("Probability Density"), **self.label_styles)
+        self.plot_widget.plotItem.setLabel("bottom", self.tr("Grain size (μm)"), **self.label_styles)
+        self.title_format = """<font face="Times New Roman">%s</font>"""
+        self.plot_widget.plotItem.setTitle(self.title_format % "Fitting Canvas")
         self.plot_widget.plotItem.showGrid(True, True)
+        self.tickFont = QFont("Arial")
+        self.tickFont.setPointSize(8)
+        self.plot_widget.plotItem.getAxis("left").tickFont = self.tickFont
+        self.plot_widget.plotItem.getAxis("bottom").tickFont = self.tickFont
         # self.plot_widget.plotItem.setMenuEnabled(False)
-        
-        self.component_styles = [
-            dict(pen=pg.mkPen("#FF5600", width=2, style=Qt.DashLine)),
-            dict(pen=pg.mkPen("#0D58A6", width=2, style=Qt.DashLine)),
-            dict(pen=pg.mkPen("#53DF00", width=2, style=Qt.DashLine)),
-            dict(pen=pg.mkPen("#BF6030", width=2, style=Qt.DashLine)),
-            dict(pen=pg.mkPen("#A63800", width=2, style=Qt.DashLine)),
-            dict(pen=pg.mkPen("#689CD2", width=2, style=Qt.DashLine)),
-            dict(pen=pg.mkPen("#CD0074", width=2, style=Qt.DashLine)),
-            dict(pen=pg.mkPen("#FFFF00", width=2, style=Qt.DashLine)),
-            dict(pen=pg.mkPen("#640CAB", width=2, style=Qt.DashLine)),
-            dict(pen=pg.mkPen("#FFCB00", width=2, style=Qt.DashLine))]
+        self.plot_widget.plotItem.setLogMode(x=True)
+        self.legend_format = """<font face="Times New Roman">%s</font>"""
+        self.legend = pg.LegendItem(offset=(80, 50))
+        self.legend.setParentItem(self.plot_widget.plotItem)
+        self.legend.addItem(self.target_item, self.legend_format % self.tr("Target"))
+        self.legend.addItem(self.sum_item, self.legend_format % self.tr("Fitted Sum"))
         self.component_curves = []
         self.component_lines = []
-        self.plot_widget.plotItem.setLogMode(x=True)
+        self.component_styles = [
+            dict(pen=pg.mkPen("#600CAC", width=2, style=Qt.DashLine)),
+            dict(pen=pg.mkPen("#0E51A7", width=2, style=Qt.DashLine)),
+            dict(pen=pg.mkPen("#FFC900", width=2, style=Qt.DashLine)),
+            dict(pen=pg.mkPen("#EA0037", width=2, style=Qt.DashLine)),
+            dict(pen=pg.mkPen("#C0F400", width=2, style=Qt.DashLine)),
+            dict(pen=pg.mkPen("#00AA72", width=2, style=Qt.DashLine)),
+            dict(pen=pg.mkPen("#53DF00", width=2, style=Qt.DashLine)),
+            dict(pen=pg.mkPen("#FF7100", width=2, style=Qt.DashLine)),
+            dict(pen=pg.mkPen("#FD0006", width=2, style=Qt.DashLine)),
+            dict(pen=pg.mkPen("#009B95", width=2, style=Qt.DashLine))]
 
-        self.legend = pg.LegendItem(offset=(50, 50))
-        self.legend.setParentItem(self.plot_widget.plotItem)
-        self.legend.addItem(self.target_item, "Target")
-        self.legend.addItem(self.sum_item, "Fitted")
 
+    # TODO: add lock because if ncomp changed during iteration, the data items will also change
     def on_ncomp_changed(self, ncomp: int):
+        self.logger.debug("Received the component changed signal, start to change clear and add data items.")
         # Check the validity of `ncomp`
         if type(ncomp) != int:
             raise TypeError(ncomp)
         if ncomp <= 0:
             raise ValueError(ncomp)
-
         # clear
         for name, curve in self.component_curves:
             self.plot_widget.plotItem.removeItem(curve)
-            self.legend.removeItem(name)
+            self.legend.removeItem(curve)
         for line in self.component_lines:
             self.plot_widget.plotItem.removeItem(line)
         self.component_curves.clear()
         self.component_lines.clear()
+        self.logger.debug("Items cleared.")
         # add
         for i in range(ncomp):
             component_name = "C{0}".format(i+1)
-            curve = pg.PlotDataItem(name=component_name,
-                                   **self.component_styles[i])
+            curve = pg.PlotDataItem(name=component_name,**self.component_styles[i])
             line = pg.InfiniteLine(angle=90, movable=False, pen=self.component_styles[i]["pen"])
             self.plot_widget.plotItem.addItem(curve)
             self.plot_widget.plotItem.addItem(line)
-            self.legend.addItem(curve, component_name)
+            self.legend.addItem(curve, self.legend_format % component_name)
             self.component_curves.append((component_name, curve))
             self.component_lines.append(line)
+        self.logger.debug("Items added.")
 
     def on_target_data_changed(self, sample_id, x, y):
         self.sample_id = sample_id
-        self.plot_widget.plotItem.setTitle(sample_id)
-        # self.target_item.setData(x, y, **self.target_style)
+        self.plot_widget.plotItem.setTitle(self.title_format % sample_id)
+        self.logger.debug("Target data has changed to %s.", sample_id)
 
     def on_epoch_finished(self, data: FittedData):
         non_nan_data = data.get_non_nan_copy()
@@ -99,6 +109,7 @@ class FittingCanvas(QWidget):
             curve_item.setData(x, y, **style)
         for i, line_item in enumerate(self.component_lines):
             line_item.setValue(math.log10(non_nan_data.statistic[i]["mean"]))
+        self.logger.debug("Epoch fitting finished. Data of DataItem has updated.")
         self.sigWidgetsEnable.emit(True)
         self.current_iteration = 0
 
@@ -113,5 +124,5 @@ class FittingCanvas(QWidget):
             curve_item.setData(x, y, **style)
         for i, line_item in enumerate(self.component_lines):
             line_item.setValue(math.log10(non_nan_data.statistic[i]["mean"]))
-        self.plot_widget.plotItem.setTitle("{0} iter({1})".format(self.sample_id, self.current_iteration))
+        self.plot_widget.plotItem.setTitle(self.title_format % "{0} iter({1})".format(self.sample_id, self.current_iteration))
         self.current_iteration += 1
