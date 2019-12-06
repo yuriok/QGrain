be32512a83dd0c780b6868311cb83fb77649e175
Yuming Liu <liuyuming@ieecas.cn>
2019-12-06 22:18:04 +0800

    add data settings

--- ui/setting_window.py
+++ ui/setting_window.py
@@ -1,36 +1,57 @@
-from PySide2.QtWidgets import QMainWindow, QCheckBox, QLabel, QRadioButton, QPushButton, QGridLayout, QApplication, QSizePolicy, QWidget, QTabWidget
-from PySide2.QtCore import Qt
-from PySide2.QtGui import QIcon
+from PySide2.QtWidgets import QMainWindow, QCheckBox, QLabel, QRadioButton, QPushButton, QGridLayout, QApplication, QSizePolicy, QWidget, QTabWidget, QComboBox, QLineEdit
+from PySide2.QtCore import Qt, QSettings, Signal
+from PySide2.QtGui import QIcon,QValidator,QIntValidator
+import sys
+
+from ui.data_setting import DataSetting
+# from data_setting import DataSetting
 
 class SettingWindow(QMainWindow):
+    sigSaveSettings = Signal(QSettings)
+    sigRestoreSettings = Signal(QSettings)
     def __init__(self):
         super().__init__()
+        self.settings = QSettings("./settings/qgrain.ini", QSettings.Format.IniFormat)
         self.init_ui()
+        self.setWindowFlags(Qt.Drawer)
+        self.setWindowTitle(self.tr("Settings"))
+        self.sigRestoreSettings.connect(self.data.restore_settings)
+        self.sigSaveSettings.connect(self.data.save_settings)
+        self.restore_button.clicked.connect(self.on_restore_clicked)
+        self.save_button.clicked.connect(self.on_save_clicked)
 
     def init_ui(self):
-        self.tab = QTabWidget(self)
-        self.setCentralWidget(self.tab)
-        self.tab.setTabPosition(QTabWidget.TabPosition.West)
+        self.central_widget = QWidget(self)
+        self.setCentralWidget(self.central_widget)
+        self.main_layout = QGridLayout(self.central_widget)
+        self.data = DataSetting(self.settings)
+        self.data.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
         
-        self.data = DataSetting()
-        self.tab.addTab(self.data, QIcon("./settings/icon.png"), None)
+        self.main_layout.addWidget(self.data, 0, 0, 1, 2)
+        self.restore_button = QPushButton(self.tr("Restore"))
+        self.save_button = QPushButton(self.tr("Save"))
+        self.main_layout.addWidget(self.restore_button, 1, 0)
+        self.main_layout.addWidget(self.save_button, 1, 1)
 
 
-class DataSetting(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.init_ui()
+    def init_settings(self):
+        self.sigRestoreSettings.emit(self.settings)
+        self.sigSaveSettings.emit(self.settings)
 
-    def init_ui(self):
-        self.main_layout = QGridLayout(self)
-        self.main_layout.addWidget(QPushButton("Hello1"), 0, 0)
-        self.main_layout.addWidget(QPushButton("Hello2"), 0, 1)
-        self.main_layout.addWidget(QPushButton("Hello3"), 0, 2)
+    def on_restore_clicked(self):
+        self.sigRestoreSettings.emit(self.settings)
+
+    def on_save_clicked(self):
+        self.sigSaveSettings.emit(self.settings)
+
 
 
 if __name__ == "__main__":
     import sys
     app = QApplication(sys.argv)
-    s = SettingWindow()
+    template_styles = open("./settings/qss/aqua.qss").read()
+    custom_style = open("./settings/custom.qss").read()
+    app.setStyleSheet(template_styles+custom_style)
+    s = SettingWindow() 
     s.show()
     app.exec_()
