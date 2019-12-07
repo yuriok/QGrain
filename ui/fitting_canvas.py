9d667d9eb2925febc254c941c3f1eb1a11c0be17
Yuming Liu <695983364@qq.com>
2019-12-07 21:48:56 +0800

    typo

--- ui/fitting_canvas.py
+++ ui/fitting_canvas.py
@@ -98,7 +98,7 @@ class FittingCanvas(QWidget):
     def on_target_data_changed(self, sample_id, x, y):
         self.sample_id = sample_id
         self.plot_widget.plotItem.setTitle(self.title_format % sample_id)
-        self.logger.debug("Target data has changed to %s.", sample_id)
+        self.logger.debug("Target data has been changed to [%s].", sample_id)
 
     def on_epoch_finished(self, data: FittedData):
         non_nan_data = data.get_non_nan_copy()
