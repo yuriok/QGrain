e6aa5eb68045eabab6527b877b82b30a36e688c7
Yuming Liu <liuyuming2024@gmail.com>
2020-03-02 19:54:46 +0800

    update

--- setup.py
+++ setup.py
@@ -12,7 +12,24 @@ setup(
     license="MIT",
     packages=find_packages(),
     include_package_data=True,
+    install_requires=[
+        'numpy>=1.18.1',
+        'PySide2>=5.14.1',
+        "scikit-learn>=0.22.1",
+        "scipy>=1.4.1",
+        "shiboken2>=5.14.1",
+        "xlrd>=1.2.0",
+        "XlsxWriter>=1.2.7",
+        "xlwt>=1.3.0",
+        "Pillow>=7.0.0",
+        "opencv-python>=4.2.0.32"
+    ],
     entry_points = {
         'console_scripts': ['qgrain=QGrain.main:main'],
-    }
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
