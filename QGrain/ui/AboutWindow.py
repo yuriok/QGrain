51aac8a7327c9b79686e0c37d3daafd8bf9d8cb0
Yuming Liu <liuyuming2024@gmail.com>
2021-04-29 03:56:13 +0800

    update about and readme

--- QGrain/ui/AboutWindow.py
+++ QGrain/ui/AboutWindow.py
@@ -8,19 +8,38 @@ from PySide2.QtWidgets import QGridLayout, QMainWindow, QTextBrowser, QWidget
 about_md = """
 # QGrain
 
-QGrain is an easy to use software that can unmix the multi-modal grain size distribution to some single modals.
+QGrain is an easy-to-use software which integrates most analysis tools to deal with grain size distributions.
 
-It's written by Python. This makes it can benefit from the great open source and scientific computation communities.
+## Tools
 
-QGrain is still during the rapid development stage, its functionalities and usages may changes many and many times. And of course, there probably are some bugs. We are very sorry for its immaturity.
+* Statistics moments
 
-We are really hope to receive your feedbacks. Whatever it's bug report, request of new feature, disscusion on algorithms.
+  Calculate samples' mean, std, skewness, kurtosis, etc. The statistics formulas were referred to Blott & Pye (2001)'s work.
 
-Moreover, we are looking forward that there are some partners to join the development of QGrain.
+* End-member analysis (EMA)
 
-If you have any idea, you can contact the authors below.
+  EMA is a widely used algorithm to extract the end-members of a whole dataset.
+  Here, QGrain provides a new implement which is based the basic Neural Network.
+
+* Single Sample Unmix (SSU)
+
+  SSU also is used to extract the end-members (i.e. components) of samples.
+  Different from EMA, it only deals with one sample at each computation.
+
+* Principal Component Analysis (PCA)
+
+  PCA can extract the major (which has the greatest variance) and minor signals of data.
+  It also be used to reduce the dimension of data.
+
+* Hierarchy Clustering
+
+  Hierarchy clustering is a set of clustering algorithms.
+  It can generate a hierarchy structure to reprsents the relationships of samples by determining their distances.
+  Using this algorithm, we can find out the typical samples and have a overall cognition of the dataset.
 
 ## Authors
+
+Feel free to contact the authors below, if you have some questions.
 
 * Yuming Liu
 
@@ -39,7 +58,7 @@ class AboutWindow(QMainWindow):
         self.setCentralWidget(self.central_widget)
         self.layout = QGridLayout(self.central_widget)
         self.setWindowTitle(self.tr("About"))
-        self.setMinimumSize(600, 400)
+        self.setMinimumSize(500, 600)
         self.text = QTextBrowser()
         self.layout.addWidget(self.text, 0, 0)
         self.text.setMarkdown(about_md)
