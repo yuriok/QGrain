74731068e5933df85cf2fe27a306e7329675df0c
Yuming Liu <liuyuming@ieecas.cn>
2021-08-30 01:16:15 +0800

    add cite information

--- QGrain/ui/AboutPanel.py
+++ QGrain/ui/AboutPanel.py
@@ -11,6 +11,10 @@ about_md = """
 
 QGrain aims to provide an easy-to-use and comprehensive analysis platform for grain-size distributions. QGrain has implemented many functions, however, there still are many useful tools that have not been contained. Hence, we published QGrain as an open-source project, and welcome other researchers to contribute their ideas and codes. Codes are available at this GitHub [repository](https://github.com/yuriok/QGrain/). More information and tutorials are available at the [offical website](https://qgrain.net/).
 
+Please cite:
+
+* Liu, Y., Liu, X., Sun, Y., 2021. QGrain: An open-source and easy-to-use software for the comprehensive analysis of grain size distributions. Sedimentary Geology 423, 105980. https://doi.org/10.1016/j.sedgeo.2021.105980
+
 Feel free to contact the author below.
 
 * Yuming Liu, a PhD student of IEECAS, [liuyuming@ieecas.cn](mailto:liuyuming@ieecas.cn)
