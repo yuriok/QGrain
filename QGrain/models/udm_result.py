64501a3404a5689b2496fef0d156b500e1219829
Yuming Liu <liuyuming2024@gmail.com>
2022-07-18 19:25:02 +0800

    refactor the `udm` submodule

--- QGrain/models/udm_result.py
+++ QGrain/models/udm_result.py
@@ -6,33 +6,31 @@ from typing import *
 import numpy as np
 from numpy import ndarray
 
-from ..distributions import DistributionType, get_distribution, get_sorted_indexes
-from ..emma import KernelType
+from ..models import DistributionType, KernelType, ArtificialDataset, Dataset
+from ..distributions import get_distribution, get_sorted_indexes
 from ..metrics import loss_numpy
-from ..models import ArtificialDataset
-from ..models import Dataset
 
 
 class UDMResult:
     def __init__(self, dataset: [ArtificialDataset, Dataset], kernel_type: KernelType, n_components: int,
                  parameters: ndarray, time_spent: Union[int, float], x0: ndarray = None,
-                 history: Sequence[ndarray] = None, settings: Dict[str, Any] = None,
-                 loss_series: Dict[str, ndarray] = None):
+                 settings: Dict[str, Any] = None, loss_series: Dict[str, ndarray] = None):
         self._dataset = dataset
         self._kernel_type = kernel_type
         self._n_components = n_components
-        self._parameters = parameters
         self._time_spent = time_spent
         self._x0 = x0
-        self._history = history
         self._settings = settings
         self._loss_series = loss_series
         self._classes_phi = np.expand_dims(np.expand_dims(self.dataset.classes_phi, axis=0), axis=0).repeat(
             self.n_samples, axis=0).repeat(self.n_components, axis=1)
         self._interval_phi = np.abs((self.dataset.classes_phi[0] - self.dataset.classes_phi[-1]) / (self.n_classes - 1))
-        self._sorted_indexes = get_sorted_indexes(self.distribution_type, parameters,
-                                                  self._classes_phi, self._interval_phi)
-        self._update(parameters)
+        indexes = get_sorted_indexes(self.distribution_type, parameters[-1], self._classes_phi, self._interval_phi)
+        sorted_parameters = np.zeros_like(parameters)
+        for i, j in enumerate(indexes):
+            sorted_parameters[:, :, :, i] = parameters[:, :, :, j]
+        self._parameters = sorted_parameters
+        self._update(-1)
 
     @property
     def dataset(self) -> Union[ArtificialDataset, Dataset]:
@@ -67,6 +65,10 @@ class UDMResult:
         return self._components
 
     @property
+    def distributions(self) -> ndarray:
+        return (self._proportions @ self._components).squeeze(1)
+
+    @property
     def time_spent(self) -> Union[int, float]:
         return self._time_spent
 
@@ -76,7 +78,7 @@ class UDMResult:
 
     @property
     def n_iterations(self) -> int:
-        return len(self._history)
+        return self._parameters.shape[0]
 
     @property
     def parameters(self) -> ndarray:
@@ -84,9 +86,9 @@ class UDMResult:
 
     @property
     def history(self):
-        for parameters in self._history:
+        for i in range(self._parameters.shape[0]):
             copy_result = copy.copy(self)
-            copy_result._update(parameters)
+            copy_result._update(i)
             yield copy_result
 
     @property
@@ -96,13 +98,10 @@ class UDMResult:
         else:
             return self._settings.copy()
 
-    def _update(self, parameters: np.ndarray):
-        sorted_parameters = np.zeros_like(parameters)
-        for i, j in enumerate(self._sorted_indexes):
-            sorted_parameters[:, :, i] = parameters[:, :, j]
+    def _update(self, index: int):
         distribution_class = get_distribution(DistributionType.__members__[self.kernel_type.name])
-        proportions, components, _ = distribution_class.interpret(sorted_parameters, self._classes_phi,
-                                                                  self._interval_phi)
+        proportions, components, _ = distribution_class.interpret(
+            self._parameters[index], self._classes_phi, self._interval_phi)
         proportions[np.logical_or(np.isnan(proportions), np.isinf(proportions))] = 0.0
         components[np.logical_or(np.isnan(components), np.isinf(components))] = 0.0
         self._proportions = proportions
@@ -115,18 +114,18 @@ class UDMResult:
         return loss_func(prediction, observation, None)
 
     def loss_series(self, name: str) -> ndarray:
-        if self.n_iterations == 0:
-            raise ValueError("No history record.")
-        else:
-            series = []
-            loss_func = loss_numpy(name)
-            observation = self._dataset.distributions
-            for result in self.history:
-                prediction = (result._proportions @ result._components).squeeze(1)
-                loss = loss_func(prediction, observation, None)
-                series.append(loss)
-            series = np.array(series)
-            return series
+        if name in self._loss_series:
+            return self._loss_series[name]
+        series = []
+        loss_func = loss_numpy(name)
+        observation = self._dataset.distributions
+        for result in self.history:
+            prediction = (result._proportions @ result._components).squeeze(1)
+            loss = loss_func(prediction, observation, None)
+            series.append(loss)
+        series = np.array(series)
+        self._loss_series[name] = series
+        return series
 
     def class_wise_losses(self, name: str) -> ndarray:
         loss_func = loss_numpy(name)
