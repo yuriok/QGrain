12fddea9086c428c7d56e527289e37ed13a8dd6e
Yuming Liu <liuyuming2024@gmail.com>
2022-12-09 15:47:16 +0800

    update tag

--- QGrain/__init__.py
+++ QGrain/__init__.py
@@ -2,7 +2,7 @@ import logging
 import os
 import socket
 
-QGRAIN_VERSION = "0.5.2.1"
+QGRAIN_VERSION = "0.5.2.2"
 QGRAIN_ROOT_PATH = os.path.dirname(__file__)
 
 HELLO_TEXT = r"""
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="QGrain",
-    version="0.5.2.1",
+    version="0.5.2.2",
     description="An easy-to-use software for the comprehensive analysis of grain size distributions",
     platforms="all",
     author="Yuming Liu",
