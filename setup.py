1802e4fa66cc7dbb7d4274b613d4dc8ce6f66a7b
Yuming Liu <liuyuming@ieecas.cn>
2023-05-15 17:28:12 +0800

    use spaces distances of samples to scale the component constraints

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="QGrain",
-    version="0.5.3.6",
+    version="0.5.4.0",
     description="An easy-to-use software for the comprehensive analysis of grain size distributions",
     platforms="all",
     author="Yuming Liu",
