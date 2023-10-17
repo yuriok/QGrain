11648b224fad09fc1e171d39b21e7ad2f08e217f
Yuming Liu <liuyuming2024@gmail.com>
2023-10-17 20:46:53 +0800

    add the citation guide for UDM

--- QGrain/assets/README.md
+++ QGrain/assets/README.md
@@ -6,6 +6,10 @@ Please cite:
 
 * Liu, Y., Liu, X., Sun, Y., 2021. QGrain: An open-source and easy-to-use software for the comprehensive analysis of grain size distributions. Sedimentary Geology 423, 105980. https://doi.org/10.1016/j.sedgeo.2021.105980
 
+If you use the universal decomposition model, please cite:
+
+* Liu, Y., Wang, T., Liu, B., Long, Y., Liu, X., Sun, Y., 2023. Universal decomposition model: An efficient technique for palaeoenvironmental reconstruction from grain-size distribution. Sedimentology. https://doi.org/10.1111/sed.13115
+
 Feel free to contact the author below.
 
 * Yuming Liu, a PhD student of IEECAS, [liuyuming@ieecas.cn](mailto:liuyuming@ieecas.cn)
--- QGrain/io/save.py
+++ QGrain/io/save.py
@@ -120,6 +120,11 @@ def _write_readme_sheet(ws: Worksheet, text: str):
         Liu, Y., Liu, X., Sun, Y., 2021. QGrain: An open-source and easy-to-use software for the comprehensive
             analysis of grain size distributions. Sedimentary Geology 423, 105980. DOI: 10.1016/j.sedgeo.2021.105980
 
+        If you use the universal decomposition model, please cite:
+        Liu, Y., Wang, T., Liu, B., Long, Y., Liu, X., Sun, Y., 2023. Universal decomposition model: An efficient
+            technique for palaeoenvironmental reconstruction from grain-size distribution.
+            Sedimentology. https://doi.org/10.1111/sed.13115
+
         {1}
 
         """.format(QGRAIN_VERSION, text)
